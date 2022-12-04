815e40ca24d2cbd7dc7dedd7d0cea5f35c953822
cha <chyb627@naver.com>
2022-12-04 23:22:45 +0900

    네비게이터 중첩하기!

--- App.tsx
+++ App.tsx
@@ -2,14 +2,38 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CategoriesScreen from './src/screens/CategoriesScreen';
 import MealsOverviewScreen from './src/screens/MealsOverviewScreen';
 import MealDetailScreen from './src/screens/MealDetailScreen';
 import DrawerScreen from './src/screens/DrawerScreen';
 import BottomtabScreen from './src/screens/BottomtabScreen';
+import FavoritesScreen from './src/screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerTintColor: 'white',
+        headerStyle: { backgroundColor: '#351401' },
+        sceneContainerStyle: { backgroundColor: '#3f2f25' },
+        // Drawer Navigation 에서는 contentStyle이 아니라 sceneContainerStyle로 설정해준다.
+      }}>
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: 'All Categories',
+        }}
+      />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -23,13 +47,11 @@ export default function App() {
             contentStyle: { backgroundColor: '#3f2f25' },
           }}>
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="DrawerScreen"
+            component={DrawerNavigator}
             options={{
               title: 'All Categories',
-              // headerTintColor: 'white',
-              // headerStyle: { backgroundColor: '#351401' },
-              // contentStyle: { backgroundColor: '#3f2f25' },
+              headerShown: false, // 드로우 네비게이터 중첩으로 헤드가 2개 생기므로 하나를 가려준다.
             }}
           />
           <Stack.Screen
@@ -45,11 +67,9 @@ export default function App() {
           <Stack.Screen
             name="MealDetail"
             component={MealDetailScreen}
-            // options={{
-            //   headerRight: () => {
-            //     return <Button title="Tab me!" onPress={} />;
-            //   },
-            // }}
+            options={{
+              title: 'About the Meal',
+            }}
           />
           <Stack.Screen name="Drawer" component={DrawerScreen} />
           <Stack.Screen name="Bottomtab" component={BottomtabScreen} />
